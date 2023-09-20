75cc21425f396f10e30ef047285bf00334d5632b
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-09-20 21:21:40 +0900

    Delete  kadai_019/event.js

---  kadai_019/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  const h2 = document.getElementById('text');
-
-  h2.textContent = 'ボタンをクリックしました';
-
-  document.querySelector('h2').appendChild(h2);
-});
